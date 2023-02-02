6ab2d0ee8a2a21aac52074753062d76ec537f006
TaewookKwak <rhkrxodnr@gmail.com>
2023-02-02 11:07:23 +0900

    test : tegit config --listst

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,6 +4,7 @@ import React from "react";
 const NavBar = () => {
   return (
     <nav>
+      <div>로고 위치</div>
       <Link href={"/"}>홈</Link>
       <Link href={"/write"}>부동산 리뷰</Link>
     </nav>
